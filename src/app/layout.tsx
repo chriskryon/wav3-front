134cfade4fd807d77a7866c8b4cb5a235314b0bc
Christopher Costa <contato.christopher@gmail.com>
2025-07-04 16:58:26 -0300

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,31 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Wav3 - Cross-border Cryptocurrency',
   description: 'Online Cross-border Cryptocurrency',
+  openGraph: {
+    title: 'Wav3 - Cross-border Cryptocurrency',
+    description: 'Online Cross-border Cryptocurrency',
+    url: 'https://staging-wav3.vercel.app/',
+    siteName: 'Wav3',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Wav3 Banner',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Wav3 - Cross-border Cryptocurrency',
+    description: 'Online Cross-border Cryptocurrency',
+    images: ['/og-image.png'],
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
