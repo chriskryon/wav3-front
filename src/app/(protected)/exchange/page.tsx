c9f8fc05ba6c7e74656aca348556c3904acd3272
Christopher Costa <contato.christopher@gmail.com>
2025-07-07 17:13:59 -0300

    style: organize exchange layout

--- src/app/(protected)/exchange/page.tsx
+++ src/app/(protected)/exchange/page.tsx
@@ -3,9 +3,9 @@ import { ExchangeQuoteForm } from '@/components/exchange/ExchangeQuoteForm';
 
 export default function ExchangePage() {
   return (
-    <div className='min-h-screen bg-gradient-to-br from-[#e6f7f8] via-white to-[#d0f3f6] flex flex-col items-center justify-center py-12 px-4'>
-      <div className='w-full max-w-2xl mx-auto'>
-        <ExchangeQuoteForm />
+    <div className='bg-gradient-to-br from-[#e6f7f8] via-white to-[#d0f3f6] flex flex-col items-center justify-center py-2 px-2 box-border h-full'>
+      <div className='w-full max-w-2xl mx-auto box-border'>
+      <ExchangeQuoteForm />
       </div>
     </div>
   );
