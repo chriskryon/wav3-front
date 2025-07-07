15cdd5037937a1d646bb6387f143b64710f3ce6a
Christopher Costa <contato.christopher@gmail.com>
2025-07-07 17:08:24 -0300

    chore add util files

---
+++ src/lib/assetIcons.tsx
@@ -0,0 +1,17 @@
+
+import { TokenBTC, TokenETH, TokenUSDC, TokenUSDT, TokenXRP } from '@web3icons/react';
+import { US, CO, MX, BR, AR, EU } from 'country-flag-icons/react/3x2';
+
+export const ICONS_CRYPTO_FIAT = {
+    BTC: TokenBTC,
+    ETH: TokenETH,
+    XRP: TokenXRP,
+    USDT: TokenUSDT,
+    USDC: TokenUSDC,
+    USD: US,
+    COP: CO,
+    MXN: MX,
+    BRL: BR,
+    EUR: EU,
+    ARS: AR,
+};
