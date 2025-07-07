c2c294556649b9278b86006ccf9551ca359d4f4f
Christopher Costa <contato.christopher@gmail.com>
2025-07-07 17:08:31 -0300

    chore add util files

---
+++ src/lib/icon-utils.ts
@@ -0,0 +1,19 @@
+import { TokenBTC, TokenETH, TokenRIF, TokenUSDC, TokenUSDT, TokenXRP } from '@web3icons/react';
+import { US, CO, MX, BR, AR, EU } from 'country-flag-icons/react/3x2';
+import { FlagIcon } from 'lucide-react';
+
+export const ICONS_CRYPTO_FIAT = {
+    "USDRIF": TokenRIF,
+    "BTC": TokenBTC,
+    "ETH": TokenETH,
+    "XRP": TokenXRP,
+    "USDT": TokenUSDT,
+    "USDC": TokenUSDC,
+    "USD": US,
+    "COP": CO,
+    "MXN": MX,
+    "BRL": BR,
+    "EUR": EU,
+    "ARS": AR,
+    "XOF": FlagIcon
+};
