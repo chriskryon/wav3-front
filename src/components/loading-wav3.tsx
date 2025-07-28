db112d69b4209153ebee270e1a183af2955c6f8a
Christopher Costa <contato.christopher@gmail.com>
2025-07-28 10:28:10 -0300

    chore: add loading component

---
+++ src/components/loading-wav3.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import logo from '../app/logo.svg';
+
+const Wav3Loading = () => (
+    <div className="flex flex-col items-center justify-center h-screen animate-pulse">
+        <Image
+            src={logo}
+            alt="WAV3 Logo"
+            width={64}
+            height={64}
+            className="object-contain transition-all duration-500 ease-in-out"
+        />
+    </div>
+);
+
+export default Wav3Loading;
